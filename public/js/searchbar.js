c74aa64e8a92bc8cc0f923fa9fb19bfe554584a2
Naveen Kumar <49795029+naveenk08@users.noreply.github.com>
2020-07-05 23:26:55 +0530

    Removed Unnecessarry things

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -5,6 +5,6 @@ handle.addEventListener('submit', (e) => {
    e.preventDefault(); //prevent refreshing after click search
     const srch = search.value
     console.log("Loading...")
-    window.history.pushState('page2', 'Title', '/?handle='+srch);
-    location.reload();
+    window.history.pushState('page2', 'Title', '/?handle='+srch);//provides an interface for manipulating the browser session history
+    location.reload();//for reloading or refreshing after pushing the page
 })
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+//main page for creating server
 const express = require('express')
 const path = require('path')
 const app = express();
-const get_data = require('./utils/cf_api.js')
+const get_data = require('./utils/cf_api.js')//get data function is here
 var fs = require('fs')
 
 const port=process.env.PORT||3000
@@ -9,21 +10,21 @@ const hbs = require('hbs')
 //setup path config
 const publicDirectory = path.join(__dirname, '../public')
 const viewpath = path.join(__dirname, '../templates/views')
-const partialpath = path.join(__dirname, '../templates/partials')
+//const partialpath = path.join(__dirname, '../templates/partials') //just for using partials
 
 //setup view and view templates
 app.set('view engine', 'hbs')
 app.set('views', viewpath);
 app.use(express.static(publicDirectory));
 
-hbs.registerPartials(partialpath) //faltu
+//hbs.registerPartials(partialpath) //for partial which i haven't used
 
 app.get('', (req, res) => {
     var handle = req.query.handle;
     console.log(handle);
     if (!handle) {
         return res.send({
-            error: "you must provide valid handle"
+            error: "May handle field is empty or Net is not connected"
         })
     }
     get_data(handle, (error, data) => {
@@ -40,15 +41,7 @@ app.get('', (req, res) => {
             g2: data.object2.g2,
 
         }
-        //  var oo=JSON.stringify(obj)
-        //  var arr=new Array()
-        //  var i=0;
-        //  for(i=0;i<data.object2.g1.lenght;i++)
-        //  {
-        //      var j=0;
-        //     //  for(j=0;j<data.object2.g1[i].lenght;j++)
-        //         arr.push(i);
-        //  }
+      
         console.log(data.object2.g1)
         //     console.log(data.object2.g1);
         var val = {
@@ -74,7 +67,7 @@ app.get('', (req, res) => {
         //res.send(val);
         res.render("index", val);
     })
-    console.log("ddd")
+    console.log("checkpoint 1")
     // res.render("index")
     //console.log(data1)
 
@@ -88,22 +81,20 @@ app.get('', (req, res) => {
 app.get('/', (req, res) => {
     res.render("index");
 })
-
+//these pages to be added soon...................
 app.get('/dashboard', (req, res) => {
     res.render("dashboard")
 })
 
 app.get('/profile', (req, res) => {
-    // if(!req.query.address){
-    //     console.log(req.query);
-    //     return res.send("glt hai bhai");
-    // }
     res.render("profile")
 })
 
 app.get('/profile/*', (req, res) => {
     res.send('404')
-}) //instead of profile in render send anohter error page
+}) 
+
+//if no page matches
 app.get('*', (req, res) => {
     res.send('404 page')
 })
